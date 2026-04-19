d54b4bed4f585bd6770d1acfc16ee48ce6b718bd
den4ik148 <dfilipisen@gmail.com>
2026-04-20 00:26:29 +0300

    0.1

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 
 #define IR_LED_PIN 6 
 #define BUTTON_PIN 5 
-
+#define ONBOARD_LED 8 
 
 struct IrCode {
   uint8_t timer_val;
@@ -28,13 +28,12 @@ uint8_t bitsLeft = 0;
 uint8_t currentBits = 0;
 uint8_t codePtr = 0;
 const IrCode* currentPowerCode = nullptr;
-uint16_t rawBuffer[512]; 
+uint16_t rawBuffer[512];
 
 bool sendRegionCodes(const IrCode* const* regionCodes, uint8_t numCodes, const char* regionName);
 
 uint8_t readBits(uint8_t count) {
   if (currentPowerCode == nullptr) return 0;
-  
   uint8_t value = 0;
   for (uint8_t i = 0; i < count; i++) {
     if (bitsLeft == 0) {
@@ -47,48 +46,46 @@ uint8_t readBits(uint8_t count) {
   return value;
 }
 
+void runJammer() {
+  digitalWrite(ONBOARD_LED, LOW);
+  while (true) {
+    unsigned long startBurst = millis();
+    while (millis() - startBurst < 200) {
+      irsend.enableIROut(38);
+      irsend.mark(500); 
+      irsend.space(500);
+      if (digitalRead(BUTTON_PIN) == LOW) {
+        goto stop_jammer;
+      }
+    }
+    delay(50);
+    if (digitalRead(BUTTON_PIN) == LOW) break;
+  }
+stop_jammer:
+  digitalWrite(ONBOARD_LED, HIGH);
+  while (digitalRead(BUTTON_PIN) == LOW) { delay(10); }
+}
+
 bool sendRegionCodes(const IrCode* const* regionCodes, uint8_t numCodes, const char* regionName) {
-  Serial.printf("\n>>> ATTACK START: %s (%d codes) <<<\n", regionName, numCodes);
-  
   for (uint8_t i = 0; i < numCodes; i++) {
     if (digitalRead(BUTTON_PIN) == LOW) {
       delay(50);
       if (digitalRead(BUTTON_PIN) == LOW) {
-        Serial.println("\n[!] ATTACK CANCELLED BY USER!");
         while(digitalRead(BUTTON_PIN) == LOW) { delay(10); } 
         return false; 
       }
     }
-
     currentPowerCode = regionCodes[i];
-    uint8_t freq = currentPowerCode->timer_val;
-    uint8_t numpairs = currentPowerCode->numpairs;
-    uint8_t bitcompression = currentPowerCode->bitcompression;
-
-    if (freq == 0) {
-      freq = 38; 
-    }
-
+    uint8_t freq = currentPowerCode->timer_val ? currentPowerCode->timer_val : 38;
     bitsLeft = 0;
     codePtr = 0;
-
-    if (numpairs * 2 > 512) {
-      Serial.println("Error: Code too large!");
-      continue;
-    }
-
-    for (uint8_t k = 0; k < numpairs; k++) {
-      uint16_t ti = (readBits(bitcompression)) * 2;
+    if (currentPowerCode->numpairs * 2 > 512) continue;
+    for (uint8_t k = 0; k < currentPowerCode->numpairs; k++) {
+      uint16_t ti = (readBits(currentPowerCode->bitcompression)) * 2;
       rawBuffer[k * 2] = currentPowerCode->times[ti] * 10;
       rawBuffer[(k * 2) + 1] = currentPowerCode->times[ti + 1] * 10;
     }
-
-    irsend.sendRaw(rawBuffer, (numpairs * 2), freq);
-    
-    if (i % 5 == 0) {
-      Serial.printf("Progress %s: %d/%d (Freq: %dkHz)\n", regionName, i + 1, numCodes, freq);
-    }
-    
+    irsend.sendRaw(rawBuffer, (currentPowerCode->numpairs * 2), freq);
     delay(50); 
   }
   return true; 
@@ -96,34 +93,32 @@ bool sendRegionCodes(const IrCode* const* regionCodes, uint8_t numCodes, const c
 
 void setup() {
   delay(3000); 
-  
   Serial.begin(115200);
   pinMode(BUTTON_PIN, INPUT_PULLUP); 
-  
-  Serial.println("\n--- SYSTEM START ---");
+  pinMode(ONBOARD_LED, OUTPUT);
+  digitalWrite(ONBOARD_LED, HIGH);
   irsend.begin();
-  
-  Serial.println("==================================");
-  Serial.println("  TV-B-Gone ESP32-S3 READY");
-  Serial.println("  Press BOOT button to start");
-  Serial.println("==================================");
 }
 
 void loop() {
   if (digitalRead(BUTTON_PIN) == LOW) {
     delay(50); 
-    if (digitalRead(BUTTON_PIN) == LOW) {
-      Serial.println("\nButton pressed. Release to start...");
-      
-      while(digitalRead(BUTTON_PIN) == LOW) { delay(10); }
-      
-      bool notCancelled = sendRegionCodes(EUpowerCodes, num_EUcodes, "Europe (EU)");
-      if (notCancelled) {
-        sendRegionCodes(NApowerCodes, num_NAcodes, "North America (NA)");
+    while(digitalRead(BUTTON_PIN) == LOW); 
+    unsigned long firstRelease = millis();
+    bool doubleClick = false;
+    while (millis() - firstRelease < 400) {
+      if (digitalRead(BUTTON_PIN) == LOW) {
+        doubleClick = true;
+        while(digitalRead(BUTTON_PIN) == LOW);
+        break;
       }
-      
-      Serial.println(">>> ATTACK FINISHED OR STOPPED <<<");
-      Serial.println("Press button again to restart.\n");
+    }
+    if (doubleClick) {
+      runJammer();
+    } else {
+      if (sendRegionCodes(EUpowerCodes, num_EUcodes, "EU")) {
+        sendRegionCodes(NApowerCodes, num_NAcodes, "NA");
+      }
     }
   }
 }
